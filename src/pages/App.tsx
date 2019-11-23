fa3981ba4d9014c17da6ce2de7b7755b79671c02
Robin <muenk@itestra.de>
2019-11-23 16:21:00 +0100

    improved practice 2

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,7 +5,7 @@ import { Info } from './Info';
 import FaceAPI from "../components/faceapi";
 import { PracticeEasy } from '../components/PracticeEasy';
 import { PracticeAdvanced } from '../components/PracticeAdvanced';
-const { Sider, Content, Header } = Layout;
+const { Sider, Content } = Layout;
 const { SubMenu, Item } = Menu;
 const { Title } = Typography;
 
