8a8bf9c8388f573183ab4cde61c935fef68c1771
Carl <carlassmann@me.com>
2019-11-23 11:07:08 +0100

    WIP Included react webcam

--- bananapp/src/components/faceapi.tsx
+++ bananapp/src/components/faceapi.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'; // let's also import Component
-import { Card } from 'antd';
+import React, { Component } from 'react';
+import Webcam from "react-webcam";
 import { Row } from 'antd';
 
 import * as faceapi from "face-api.js";
@@ -15,37 +15,44 @@ interface IFaceAPIState {
 }
 
 export default class FaceAPI extends Component<{}, IFaceAPIState> {
+    webcamId: string = "webcam";
+    _child: React.RefObject<Webcam>;
     constructor(props: {}) {
         super(props);
-        this.state = {ready: false};
+        this.state = { ready: false };
+        this._child = React.createRef();
         this.loadModel();
     }
 
     async loadModel() {
-        const MODEL_URL = '../../models/';
-        debugger;
+        const MODEL_URL = '/models';
         await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
         await faceapi.loadFaceLandmarkModel(MODEL_URL)
         await faceapi.loadFaceRecognitionModel(MODEL_URL)
-        this.setState({ready: true});
+        this.setState({ ready: true });
     }
 
-    // applyModel() {
-    //     if (!this.state.ready) {
-    //         return;
-    //     }
-    //     let faceDescriptions = faceapi.detectSingleFace()
-    // }
+    applyModel() {
+        if (!this.state.ready) {
+            return;
+        }
+        const node = this._child.current;
+        if (!node) {
+            return;
+        }
+        const videoElement = node.getScreenshot();
+        
+        // let faceDescriptions = faceapi.detectSingleFace().withFaceExpressions()
+    }
+
+    async run() {
+        return;
+    }
 
     render() {
         return <div style={centerStyle}>
             <Row>
-                <Card
-                    hoverable
-                    style={{ width: 240 }}
-                    cover={<img alt="example" src="../../images/glasses-for-round-faces.jpg" />}
-                >
-                </Card>
+                <Webcam mirrored={true} id={this.webcamId} ref={this._child}></Webcam>
             </Row>
         </div>
     }
