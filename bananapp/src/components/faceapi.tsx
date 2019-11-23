7f90d5a96d297f36e16993a187835e09c66e802a
Carl <carlassmann@me.com>
2019-11-23 12:51:13 +0100

    Working FaceAPI component

--- bananapp/src/components/faceapi.tsx
+++ bananapp/src/components/faceapi.tsx
@@ -12,47 +12,74 @@ const centerStyle = {
 interface IFaceAPIState {
     emotion?: string;
     ready: boolean;
+    started: boolean;
 }
 
 export default class FaceAPI extends Component<{}, IFaceAPIState> {
     webcamId: string = "webcam";
+    canvasId: string = "overlay";
     _child: React.RefObject<Webcam>;
     constructor(props: {}) {
         super(props);
-        this.state = { ready: false };
+        this.state = { ready: false, started: false };
         this._child = React.createRef();
-        this.loadModel();
     }
 
     async loadModel() {
         const MODEL_URL = '/models';
-        await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
-        await faceapi.loadFaceLandmarkModel(MODEL_URL)
-        await faceapi.loadFaceRecognitionModel(MODEL_URL)
+        await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+        await faceapi.loadAgeGenderModel(MODEL_URL);
+        await faceapi.loadFaceExpressionModel(MODEL_URL);
         this.setState({ ready: true });
+        console.log("loaded models", faceapi)
     }
 
-    applyModel() {
+    async applyModel(node: Webcam, videoElement: HTMLVideoElement, canvas: HTMLCanvasElement) {
+        const detections = await faceapi.detectSingleFace(videoElement).withFaceExpressions()
+        if (!detections) {
+            console.log("Detections are undefined :-( Model is not ready yet?")
+        } else {
+            const dims = faceapi.matchDimensions(canvas, videoElement, true);
+            const resizedResult = faceapi.resizeResults(detections, dims);
+            if (resizedResult) {
+                faceapi.draw.drawDetections(canvas, resizedResult);
+                faceapi.draw.drawFaceExpressions(canvas, resizedResult, 0.05)
+            }
+            let maxConfidenceEmotion = "neutral";
+            let confidence = 0.0;
+            for (let [emotion, newConfidence] of Object.entries(detections.expressions)) {
+                if (newConfidence > confidence) {
+                    maxConfidenceEmotion = emotion;
+                    confidence = newConfidence;
+                }
+            }
+            console.log(maxConfidenceEmotion);
+            this.setState({ emotion: maxConfidenceEmotion })
+        }
+        setTimeout(() => this.applyModel(node, videoElement, canvas))
+    }
+
+    async startModel() {
+        this.setState({ started: true });
         if (!this.state.ready) {
-            return;
+            await this.loadModel();
         }
+        const videoElement = document.getElementById(this.webcamId) as HTMLVideoElement
+        const canvas = document.getElementById(this.canvasId) as HTMLCanvasElement
         const node = this._child.current;
-        if (!node) {
-            return;
+        if (node) {
+            console.log("starting face detection loop")
+            await this.applyModel(node, videoElement, canvas);
         }
-        const videoElement = node.getScreenshot();
-        
-        // let faceDescriptions = faceapi.detectSingleFace().withFaceExpressions()
     }
-
-    async run() {
-        return;
-    }
-
     render() {
-        return <div style={centerStyle}>
+        if (!this.state.started) {
+            this.startModel();
+        }
+        return <div style={centerStyle} className={"webcam-component"}>
             <Row>
-                <Webcam mirrored={true} id={this.webcamId} ref={this._child}></Webcam>
+                <Webcam id={this.webcamId} ref={this._child} hidden={false}></Webcam>
+                <canvas style={{ position: "absolute", top: "0px", left: "0px" }} id={this.canvasId}></canvas>
             </Row>
         </div>
     }
